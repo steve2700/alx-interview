2cc691b5fadb3895431927d2b4aac9ab9c3793a7
steve2700 <stevnyaru@github.com>
2023-11-21 01:44:18 -0800

    reformatted 0-making_change.py fixed trailing whitespaces using black

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,20 +3,27 @@
 
 
 def makeChange(coins, total):
-    """Determine the fewest number of coins needed to meet a given amount total
-    Return: fewest number of coins needed to meet total
+    """Determine the fewest number of coins
+    needed to meet a given amount total
+    Return: fewest number of
+    coins needed to meet total
     If total is 0 or less, return 0
-    If total cannot be met by any number of coins you have, return -1
-    coins is a list of the values of the coins in your possession
-    The value of a coin will always be an integer greater than 0
-    You can assume you have an infinite number of each
+    If total cannot be met by
+    any number of coins you have, return -1
+    coins is a list of the values
+    of the coins in your possession
+    The value of a coin will always
+    be an integer greater than 0
+    You can assume you have
+    an infinite number of each
     denomination of coin in the list
     """
     if total <= 0:
         return 0
 
-    # Initialize an array to store the minimum number of coins needed for each amount
-    dp = [float('inf')] * (total + 1)
+    # Initialize an array to store the minimum
+    # number of coins needed for each amount
+    dp = [float("inf")] * (total + 1)
     dp[0] = 0
 
     # Iterate through each coin value
@@ -24,4 +31,4 @@ def makeChange(coins, total):
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != float("inf") else -1
