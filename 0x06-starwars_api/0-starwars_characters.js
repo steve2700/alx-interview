badf637356ddcef7e4dbb0e7df368dd83ee6ee59
steve2700 <stevnyaru@github.com>
2024-01-22 12:54:32 +0200

    executable files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,31 @@
 #!/usr/bin/node
-/*
-Write a script that prints all characters of a Star Wars movie:
-
-The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
-Display one character name per line in the same order as the “characters” list in the /films/ endpoint
-*/
 
 const request = require('request');
-const util = require('util');
-const requestPromise = util.promisify(request);
-const movieId = process.argv[2];
 
-async function movieCharacter () {
-  try {
-    const response = await requestPromise('https://swapi-api.alx-tools.com/api/films/' + movieId);
-    const responseParse = JSON.parse(response.body);
-    for (const character of responseParse.characters) {
-      const characterResponse = await requestPromise(character);
-      const characterResponseParse = JSON.parse(characterResponse.body);
-      console.log(characterResponseParse.name);
-    }
-  } catch (error) {
-    console.error(error);
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
   }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-movieCharacter();
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
